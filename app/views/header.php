ca2ce711b976d9f533c83be1fae6311c7e3ce00f
junyuifeng <678234@student.inholland.nl>
2023-02-12 13:48:07 +0100

    nav-items to right alligned and background color for navbar added

--- app/views/header.php
+++ app/views/header.php
@@ -12,7 +12,7 @@
         />
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+<nav class="navbar navbar-expand-lg navbar-dark mb-4">
   <div class="container">
     <a class="navbar-brand" href="/">Haarlem</a >
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
@@ -20,7 +20,7 @@
     </button>
     
     <div class="collapse navbar-collapse" id="navbarCollapse">
-      <ul class="navbar-nav me-auto mb-2 mb-md-0">
+      <ul class="nav navbar-nav ms-auto">
         <li class="nav-item">
           <a class="nav-link" href="/">Home</a>
         </li>
