37f7f94ae57d591c6d4f558de5c4ac523da692e4
Kabir <658116@student.inholland.nl>
2023-02-17 16:02:38 +0100

    visit haarlem for history part done without the card background color and text color

--- app/views/visithaarlem/history.php
+++ app/views/visithaarlem/history.php
@@ -1,18 +1,121 @@
-<?php
-include __DIR__ . '/../header.php';
-?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>History homepage</title>
+    <link rel="stylesheet"
+              type="text/css"
+              href="/css/style.css"
+        />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
-<body>
 
-<H1>History</H1>
+<body>
+<?php
+    include __DIR__ . '/../header.php';
+    ?>
     
+    <h1 class="container mt-4">Haarlem<br> for history</h1>
+    <section class="container mt-5 mb-5">
+        <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="/img/History-Banner.png" alt="First slide">
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <section class="container mb-5">
+        <h1>Haarlem history</h1>
+        <p>Haarlem is a city and municipality at the same time located in the bank of river Spaarne. It is the capital of North Holland. It is a medium size city with 162,914 inhabitants. The neighboring cities of Haarlem are Bloemendaal, Hoofddorp, Zandvoort
+            and Haarlemmermeer. This city was first mentioned in a document from the 10th century. In 1245 Haarlem received its recognition as a city from William II of Haarlem. This city has a rich history with old architectural buildings such as De
+            Adriaan, Meat Hall, Taylers Hofje, The Dome etc. which attract tourists to visit the city.
+        </p>
+    </section>
+
+    <section class="container mb-5">
+        <div class="row gy-3">
+            <div class="card col me-4" style="width: 18rem;">
+                <img class="card-img-top p-3" src="/img/History-Card1.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">De Adriaan
+                    </h5>
+                    <p class="card-text">De Adriaan a Wind Mill was built in 1778 by Adriaan de Boois (an Amsterdam enterpreneur). On may 19, 1779 , the mill started to use for grinding tuff into trass. Tras is added to masonry motor to make walls water light. In 1802, this
+                        mill was sold to a tobacconist, and he turn this mill into a tobacco factory.
+                    </p>
+
+                </div>
+            </div>
+            <div class="card col" style="width: 18rem;">
+                <img class="card-img-top p-3" src="/img/History-Card2.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">St. Bavokerk
+                    </h5>
+                    <p class="card-text">The Grote or St.-Bavokerk (Historical Sint-Bavo Cathedral ) is a late medieval church building in the Dutch city of Haarlem, located on the Grote Markt. It was dedicated to Saint Bavo until the Reformation..
+                    </p>
+
+                </div>
+            </div>
+            <div class="w-100"></div>
+            <div class="card col me-4" style="width: 18rem;">
+                <img class="card-img-top p-3" src="/img/History-Card3.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Dome Prison
+                    </h5>
+                    <p class="card-text">The dome prison is a former prison in Haarlem. It is one of three Panopticon-style buildings situated in the country. It was built in 1901 by Willem Metzelaar.
+                    </p>
+
+                </div>
+            </div>
+            <div class="card col" style="width: 18rem;">
+                <img class="card-img-top p-3" src="/img/History-Card4.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Teylers Museum
+                    </h5>
+                    <p class="card-text"> Teylers Museum is an art, natural history, and science museum in Haarlem, Netherlands. Established in 1778, Teylers Museum was founded as a centre for contemporary art and science.
+                    </p>
+
+                </div>
+            </div>
+            <div class="w-100"></div>
+            <div class="card col me-4" style="width: 18rem;">
+                <img class="card-img-top p-3" src="/img/History-Card5.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Frans Hals Museum
+                    </h5>
+                    <p class="card-text">The Frans Hals Museum ( Frans Halsmuseum , formerly Stedelijk Museum van Haarlem ) is a museum in the North Holland city of Haarlem , founded in 1862, known as "museum of the Golden Age ". The collection is based on the rich collection
+                        of the city itself, which has been built up since the 16th century.
+
+                    </p>
+
+                </div>
+            </div>
+            <div class="card col" style="width: 18rem;">
+                <img class="card-img-top p-3" src="/img/History-Card6.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Hannie Schaft Memoria
+                    </h5>
+                    <p class="card-text">Jannetje Johanna (Jo) Schaft (16 September 1920 – 17 April 1945) was a Dutch resistance fighter during World War II. She became known as "the girl with the red hair" (Dutch: het meisje met het rode haar, German: das Mädchen mit dem
+                        roten Haar). Her secret name in the resistance movement was "Hannie".
+
+                    </p>
+
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
 </body>
+
 </html>
