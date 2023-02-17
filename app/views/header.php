5ce5a6c201e0fbb539fd9c971903c6f7d096cfd3
junyuifeng <678234@student.inholland.nl>
2023-02-17 12:03:30 +0100

    nav-item resize

--- app/views/header.php
+++ app/views/header.php
@@ -10,30 +10,44 @@
     <div class="collapse navbar-collapse" id="navbarCollapse">
       <ul class="nav navbar-nav ms-auto" id="navigation">
         <li class="nav-item">
-          <a class="nav-link" href="/"><b>Home</b></a>
+          <a class="nav-link" href="/">
+            <h3><b>Home</b></h3>
+          </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/visithaarlem/history"><b>History</b></a>
+          <a class="nav-link" href="/visithaarlem/history">
+            <h3><b>History</b></h3>
+          </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/visithaarlem/culture"><b>Culture</b></a>
+          <a class="nav-link" href="/visithaarlem/culture">
+            <h3><b>Culture</b></h3>
+          </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/visithaarlem/food"><b>Food</b></a>
+          <a class="nav-link" href="/visithaarlem/food">
+            <h3><b>Food</b></h3>
+          </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/visithaarlem/kids"><b>Kids</b></a>
+          <a class="nav-link" href="/visithaarlem/kids">
+            <h3><b>Kids</b></h3>
+          </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/visithaarlem/festival"><b>Festival</b></a>
+          <a class="nav-link" href="/visithaarlem/festival">
+            <h3><b>Festival</b></h3>
+          </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/visithaarlem/personalprogram"><b>Personal Program</b></a>
+          <a class="nav-link" href="/visithaarlem/personalprogram">
+            <h3><b>personal program</b></h3>
+          </a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="/cart/index">
-            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
-              <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-cart-fill" viewBox="0 0 16 16">
+              <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
             </svg>
           </a>
         </li>
