34341026771443dcda09fbc4590f9a2a1388df78
Kabir <658116@student.inholland.nl>
2023-02-19 23:18:23 +0100

    haarlem for history front end part done

--- app/views/visithaarlem/history.php
+++ app/views/visithaarlem/history.php
@@ -19,7 +19,7 @@
     include __DIR__ . '/../header.php';
     ?>
     
-    <h1 class="container mt-4">Haarlem<br> for history</h1>
+    <h1 class="container mt-4 heading">Haarlem<br> for history</h1>
     <section class="container mt-5 mb-5">
         <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
             <div class="carousel-inner">
@@ -32,7 +32,7 @@
     </section>
 
     <section class="container mb-5">
-        <h1>Haarlem history</h1>
+        <h1 class="heading">Haarlem history</h1>
         <p>Haarlem is a city and municipality at the same time located in the bank of river Spaarne. It is the capital of North Holland. It is a medium size city with 162,914 inhabitants. The neighboring cities of Haarlem are Bloemendaal, Hoofddorp, Zandvoort
             and Haarlemmermeer. This city was first mentioned in a document from the 10th century. In 1245 Haarlem received its recognition as a city from William II of Haarlem. This city has a rich history with old architectural buildings such as De
             Adriaan, Meat Hall, Taylers Hofje, The Dome etc. which attract tourists to visit the city.
@@ -41,67 +41,67 @@
 
     <section class="container mb-5">
         <div class="row gy-3">
-            <div class="card col me-4" style="width: 18rem;">
+            <div class="card-bg-color col me-4 rounded p-3" style="width: 18rem;">
                 <img class="card-img-top p-3" src="/img/History-Card1.png" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">De Adriaan
+                <div class="card-body p-3">
+                    <h5 class="card-text-color">De Adriaan
                     </h5>
-                    <p class="card-text">De Adriaan a Wind Mill was built in 1778 by Adriaan de Boois (an Amsterdam enterpreneur). On may 19, 1779 , the mill started to use for grinding tuff into trass. Tras is added to masonry motor to make walls water light. In 1802, this
+                    <p class="card-text-color">De Adriaan a Wind Mill was built in 1778 by Adriaan de Boois (an Amsterdam enterpreneur). On may 19, 1779 , the mill started to use for grinding tuff into trass. Tras is added to masonry motor to make walls water light. In 1802, this
                         mill was sold to a tobacconist, and he turn this mill into a tobacco factory.
                     </p>
 
                 </div>
             </div>
-            <div class="card col" style="width: 18rem;">
+            <div class="card-bg-color col rounded p-3" style="width: 18rem;">
                 <img class="card-img-top p-3" src="/img/History-Card2.png" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">St. Bavokerk
+                <div class="card-body p-3">
+                    <h5 class="card-text-color">St. Bavokerk
                     </h5>
-                    <p class="card-text">The Grote or St.-Bavokerk (Historical Sint-Bavo Cathedral ) is a late medieval church building in the Dutch city of Haarlem, located on the Grote Markt. It was dedicated to Saint Bavo until the Reformation..
+                    <p class="card-text card-text-color">The Grote or St.-Bavokerk (Historical Sint-Bavo Cathedral ) is a late medieval church building in the Dutch city of Haarlem, located on the Grote Markt. It was dedicated to Saint Bavo until the Reformation..
                     </p>
 
                 </div>
             </div>
             <div class="w-100"></div>
-            <div class="card col me-4" style="width: 18rem;">
+            <div class="card-bg-color col me-4 rounded p-3" style="width: 18rem;">
                 <img class="card-img-top p-3" src="/img/History-Card3.png" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">Dome Prison
+                <div class="card-body p-3">
+                    <h5 class="card-text-color">Dome Prison
                     </h5>
-                    <p class="card-text">The dome prison is a former prison in Haarlem. It is one of three Panopticon-style buildings situated in the country. It was built in 1901 by Willem Metzelaar.
+                    <p class="card-text card-text-color">The dome prison is a former prison in Haarlem. It is one of three Panopticon-style buildings situated in the country. It was built in 1901 by Willem Metzelaar.
                     </p>
 
                 </div>
             </div>
-            <div class="card col" style="width: 18rem;">
+            <div class="card-bg-color col rounded p-3" style="width: 18rem;">
                 <img class="card-img-top p-3" src="/img/History-Card4.png" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">Teylers Museum
+                <div class="card-body p-3">
+                    <h5 class="card-text-color">Teylers Museum
                     </h5>
-                    <p class="card-text"> Teylers Museum is an art, natural history, and science museum in Haarlem, Netherlands. Established in 1778, Teylers Museum was founded as a centre for contemporary art and science.
+                    <p class="card-text card-text-color"> Teylers Museum is an art, natural history, and science museum in Haarlem, Netherlands. Established in 1778, Teylers Museum was founded as a centre for contemporary art and science.
                     </p>
 
                 </div>
             </div>
             <div class="w-100"></div>
-            <div class="card col me-4" style="width: 18rem;">
+            <div class="card-bg-color col me-4 rounded p-3" style="width: 18rem;">
                 <img class="card-img-top p-3" src="/img/History-Card5.png" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">Frans Hals Museum
+                <div class="card-body p-3">
+                    <h5 class="card-text-color">Frans Hals Museum
                     </h5>
-                    <p class="card-text">The Frans Hals Museum ( Frans Halsmuseum , formerly Stedelijk Museum van Haarlem ) is a museum in the North Holland city of Haarlem , founded in 1862, known as "museum of the Golden Age ". The collection is based on the rich collection
+                    <p class="card-text card-text-color">The Frans Hals Museum ( Frans Halsmuseum , formerly Stedelijk Museum van Haarlem ) is a museum in the North Holland city of Haarlem , founded in 1862, known as "museum of the Golden Age ". The collection is based on the rich collection
                         of the city itself, which has been built up since the 16th century.
 
                     </p>
 
                 </div>
             </div>
-            <div class="card col" style="width: 18rem;">
+            <div class="card-bg-color col rounded p-3" style="width: 18rem;">
                 <img class="card-img-top p-3" src="/img/History-Card6.png" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">Hannie Schaft Memoria
+                <div class="card-body p-3">
+                    <h5 class="card-text-color">Hannie Schaft Memoria
                     </h5>
-                    <p class="card-text">Jannetje Johanna (Jo) Schaft (16 September 1920 – 17 April 1945) was a Dutch resistance fighter during World War II. She became known as "the girl with the red hair" (Dutch: het meisje met het rode haar, German: das Mädchen mit dem
+                    <p class="card-text card-text-color">Jannetje Johanna (Jo) Schaft (16 September 1920 – 17 April 1945) was a Dutch resistance fighter during World War II. She became known as "the girl with the red hair" (Dutch: het meisje met het rode haar, German: das Mädchen mit dem
                         roten Haar). Her secret name in the resistance movement was "Hannie".
 
                     </p>
@@ -112,7 +112,9 @@
         </div>
     </section>
 
-
+    <?php
+    include __DIR__ . '/../footer.php';
+    ?>
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
