10177c7d1725d03438fa56f45e424f81f244f263
junyuifeng <678234@student.inholland.nl>
2023-02-23 16:08:27 +0100

    user model update

--- app/model/User.php
+++ app/model/User.php
@@ -1,8 +1,10 @@
 <?php
 class User
 {
-    private $username;
-    private $password;
+    private string $username;
+    private string $email;
+    private string $password;
+    private string $userType;
 
     public function __construct($username, $password)
     {
@@ -17,5 +19,85 @@ class User
         } else {
             return false;
         }
+    }
+
+    /**
+     * Get the value of username
+     */ 
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set the value of username
+     *
+     * @return  self
+     */ 
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */ 
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */ 
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of password
+     */ 
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set the value of password
+     *
+     * @return  self
+     */ 
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of userType
+     */ 
+    public function getUserType()
+    {
+        return $this->userType;
+    }
+
+    /**
+     * Set the value of userType
+     *
+     * @return  self
+     */ 
+    public function setUserType($userType)
+    {
+        $this->userType = $userType;
+
+        return $this;
     }
 }
