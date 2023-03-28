cbac10560442c5a85612754cb895d2b51c9c7832
junyuifeng <678234@student.inholland.nl>
2023-03-28 23:09:59 +0200

    cart update

--- app/controllers/cartcontroller.php
+++ app/controllers/cartcontroller.php
@@ -29,10 +29,6 @@ class CartController
 
         if (isset($_SESSION["cart"])&&!isset($_SESSION["logedin"])) {
             $items = $_SESSION["cart"];
-
-            foreach ($_SESSION["cart"] as $item) {
-                $totalAmount += $item["price"];
-            }
         }
         else
         {
@@ -40,23 +36,6 @@ class CartController
             $userId = $loggedInUser->getId();
             $items = $this->reservationService->getFromCartByUserId($userId);
         }
-
-        if (isset($_SESSION["cart"])&&!isset($_SESSION["logedin"])) {
-            foreach ($_SESSION["cart"] as $item) {
-                $totalAmount += $item["price"];
-            }
-        }
-        else
-        {
-            $loggedInUser = $this->userService->getById($_SESSION["logedin"]);
-            $userId = $loggedInUser->getId();
-            $items = $this->reservationService->getFromCartByUserId($userId);
-            foreach ($items as $item) {
-                $quantity = $item->getAmountAbove12();
-                
-            }
-        }
-        return $totalAmount;
 
         //var_dump($items);
         //session_destroy();
