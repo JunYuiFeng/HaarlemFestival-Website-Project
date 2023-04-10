0524257a2b1e10088052a9e227225d674a1df257
Sahib <sahibzulfigar4@gmail.com>
2023-04-10 12:47:26 +0200

    services code refactoring

--- app/services/userservice.php
+++ app/services/userservice.php
@@ -168,6 +168,34 @@ class UserService
         return FALSE;
     }
 
+    public function login(string $username, string $password): string
+    {
+        $users = $this->repository->getAll();
+        $loginUser = NULL;
+
+        foreach ($users as $user) {
+            if ($username == $user->getUsername()) {
+                $loginUser = $user;
+                break;
+            } else if ($username == $user->getEmail()) {
+                $loginUser = $user;
+                break;
+            }
+        }
+
+        if ($loginUser && password_verify($password, $loginUser->getPassword())) {
+            return true;
+        }
+        return false;
+    }
+
+    public function logout()
+    {
+        unset($_SESSION["logedin"]);
+        if (isset($_SESSION["employee"]))
+            unset($_SESSION["employee"]);
+    }
+
 
 
     public function checkUsernameAvailability($username, $id = NULL): bool
